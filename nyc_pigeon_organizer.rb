a92dc4587d0a7ce90bcdc531547744415bc3ffe9
Laura Berge <lauraeberge@gmail.com>
2019-08-30 17:30:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,11 @@ def nyc_pigeon_organizer(data)
     values.each do |value, array|
       array.each do |name|
         if pigeons[name] == nil
-          
+          pigeons[name] = {}
+          pigeons[name][category] = {}
+        elsif pigeons[name]
+        
+        end
       end
     end
   end
