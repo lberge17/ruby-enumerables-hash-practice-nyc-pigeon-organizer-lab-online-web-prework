51b8bb212665135fef84afcb709bd45db5944026
Laura Berge <lauraeberge@gmail.com>
2019-08-30 18:06:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,22 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
-  data.each do |category, values|
+  data.each do |categories, values|
     values.each do |value, array|
       array.each do |name|
         if pigeons[name] == nil
           pigeons[name] = {}
-          pigeons[name][category] = []
+          pigeons[name][categories] = []
         else
-          pigeons[name][category] = []
+          pigeons[name][categories] = []
         end
       end
     end
   end
-  pigeons.each do |name, values|
-    values.each do |pigeonvalue, array|
-      data.each do |category, values|
-        values.each do |value, array|
-          array.each do |element|
+  pigeons.each do |name, pigeon_categories|
+    pigeon_categories.each do |pigeon_category, array|
+      data.each do |categories, values|
+        values.each do |value, name_array|
+          name_array.each do |element|
             if element == name && pigeonvalue == category
               pigeons[name][pigeonvalue] << value.to_s
             end
