4ebd6bf3cfd856b7af6bea527f85235e5273269d
Laura Berge <lauraeberge@gmail.com>
2019-08-30 17:59:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,21 @@
 pigeon_data = {
   :color => {
-    :brown => ["Luca"],
-    :black => ["Lola"],
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
   },
   :gender => {
-    :male => ["Luca"],
-    :female => ["Lola"]
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
   },
   :lives => {
-    "Central Park" => ["Lola"],
-    "Library" => ["Luca"]
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
   }
 }
-
-
 
 
 def nyc_pigeon_organizer(data)
