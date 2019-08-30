504a8a43865d4ad8c728bdda03c63d62e4eac229
Laura Berge <lauraeberge@gmail.com>
2019-08-30 18:39:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |data_category, data_values|
+    data_values.each do |data_value, data_array|
+      data_array.each do |data_name|
+        if pigeons[data_name] == nil
+          pigeons[data_name] = {}
+          pigeons[data_name][data_category] = []
+        else
+          pigeons[data_name][data_category] = []
+        end
+      end
+    end
+  end
+  pigeons.each do |pigeons_name, pigeons_categories|
+    pigeons_categories.each do |pigeons_category, pigeons_array|
+      data.each do |data_categories, data_values|
+        data_values.each do |data_value, data_array|
+          data_array.each do |data_name|
+            if data_name == pigeons_name && pigeons_category == data_categories
+              pigeons[pigeons_name][pigeons_category] << data_value.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  pigeons
 end
